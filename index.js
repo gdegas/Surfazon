64a7596d07667924483eb82215325826bc49060e
gdegas001 <gdegas001@gmail.com>
2017-09-29 17:02:45 -0700

    Added process.env port for heroku

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname, '/index.html')
 })
 
-app.listen(3000, () => console.log('listening on 3000'))
+app.listen(process.env.PORT || 3000, () => console.log('listening on 3000'))
